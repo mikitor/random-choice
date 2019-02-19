3e1e7ff47859c5265c7522809ef5f352273350d3
Nick Torbich <nicktorbich@protonmail.com>
2019-02-19 02:42:34 +0100

    Add style to the Options component

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,10 +4,17 @@ import Option from './Option';
 
 const Options = props => (
   <div>
-    {props.options.length === 0 && <p>Please get an option to get started! :)</p>}
-    <button onClick={props.handleDeleteAll}>Remove All</button>
+    <div className="main-header">
+      <h3 className="main-header__title">Your Options</h3>
+      <button className="button button--delete" type="button" onClick={props.handleDeleteAll}>
+        Delete All
+      </button>
+    </div>
+    {props.options.length === 0 && (
+      <p className="main-message">Please get an option to get started! :)</p>
+    )}
     {props.options.map((option, index) => {
-      return <Option key={index} id={index} text={option} handleDelete={props.handleDelete} />
+      return <Option key={index} id={index} text={option} handleDelete={props.handleDelete} />;
     })}
   </div>
 );
