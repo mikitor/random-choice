e85a5d1aef19f60397467ffa6a34e04f8411a047
Nick Torbich <nicktorbich@protonmail.com>
2019-02-19 02:36:09 +0100

    Add style to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const Header = props => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
+  <div className="header">
+    <div className="container">
+      <h1 className="header__title">{props.title}</h1>
+      {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+    </div>
   </div>
 );
 
 Header.defaultProps = {
-  title: 'Some Title',
+  title: 'Random choice'
 };
 
 export default Header;
